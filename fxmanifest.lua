b05e3997b10a60b4c04f850e3dec5afc9d7743ba
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2023-03-02 17:12:18 -0500

    Main files added

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+
+author 'Project Sloth'
+
+discord 'https://discord.gg/projectsloth'
+
+game 'gta5'
+
+version '1.0'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_bubblegum.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_cherry.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_chocolate.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_coffee.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_doublechocolate.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_frappe.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_lemon.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_mint.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_raspberry.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_rsaltedcaramel.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_strawberry.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/brum_shake_vanilla.ytyp'
